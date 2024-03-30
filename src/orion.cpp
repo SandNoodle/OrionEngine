620fed5789da2cc516b10f9e5976949d57b76200
SandNoodle <cddgamer@gmail.com>
2024-03-30 22:03:21 +0100

    Add `application_t` for Orion...
    ... provides application_callbacks_t interface for the user to hook up
    to.

---
+++ src/orion.cpp
@@ -0,0 +1,89 @@
+#include "orion.h"
+
+#include "platform/timer.h"
+
+#include "core/assert.h"
+#include "core/math/omath.h"
+
+namespace orion
+{
+	application_t application_t::create(application_desc_t desc, application_callbacks_t callbacks)
+	{
+		application_t app;
+		app._desc      = desc;
+		app._callbacks = callbacks;
+		app._window    = window_t::create(desc.window_desc);
+		return app;
+	}
+
+	void application_t::destroy()
+	{
+		_window.destroy();
+	}
+
+	void application_t::start()
+	{
+		_callbacks.start(_callbacks.user_data);
+	}
+
+	void application_t::stop()
+	{
+		_callbacks.stop(_callbacks.user_data);
+	}
+
+	void application_t::run()
+	{
+		u32 updates_this_second = 0;
+		u32 frames_this_second = 0;
+
+		f64 last_time = platform_get_absolute_time();
+		f64 timer = last_time;
+		f64 accumulator = 0.0f;
+		const f64 max_update_time = 1.0f / _desc.updates_per_second;
+		while(!_window.get_should_close())
+		{
+			_window.poll_events();
+			f64 current_time = platform_get_absolute_time();
+			f64 delta_time = math::min(current_time - last_time, max_update_time);
+			last_time = current_time;
+
+			accumulator += delta_time;
+			while(accumulator > max_update_time)
+			{
+				accumulator -= max_update_time;
+				updates_this_second++;
+				_callbacks.update(max_update_time, _callbacks.user_data);
+			}
+
+			frames_this_second++;
+			_callbacks.render(_callbacks.user_data);
+			_window.swap_buffers();
+
+			if(current_time - timer > 1.0f)
+			{
+				OE_LOG_TRACE("UPS: %d, FPS: %d, FRAME_TIME: %f ms",
+					updates_this_second,
+					frames_this_second,
+					1000.0f / frames_this_second);
+				timer++;
+				frames_this_second = 0;
+				updates_this_second = 0;
+			}
+		}
+	}
+
+
+	void orion_main(application_t* app)
+	{
+		OE_ASSERT_TRUE(app, "orion_main: failed to provide the application_t struct.");
+		OE_ASSERT_TRUE(app->_callbacks.user_data, "orion_main: failed to provide user_data.");
+		OE_ASSERT_TRUE(app->_callbacks.start,     "orion_main: failed to provide the 'start' callback.");
+		OE_ASSERT_TRUE(app->_callbacks.stop,      "orion_main: failed to provide the 'stop' callback.");
+		OE_ASSERT_TRUE(app->_callbacks.update,    "orion_main: failed to provide the 'update' callback.");
+		OE_ASSERT_TRUE(app->_callbacks.render,    "orion_main: failed to provide the 'render' callback.");
+		app->start();
+		app->run();
+		app->stop();
+		app->destroy();
+	}
+}
--- src/orion.h
+++ src/orion.h
@@ -1,5 +1,59 @@
 #pragma once
 
 #include "platform/types.h"
-
 #include "core/builds.h"
+
+#include "platform/graphics/window.h"
+
+namespace orion
+{
+	struct application_callbacks_t
+	{
+		void* user_data;
+
+		/**
+		 * @brief
+		 * @param user_data
+		 */
+		void (*start)(void* user_data);
+
+		/**
+		 * @brief
+		 * @param user_data
+		 */
+		void (*stop)(void* user_data);
+
+		/**
+		 * @brief
+		 * @param delta_time
+		 * @param user_data
+		 */
+		void (*update)(f32 delta_time, void* user_data);
+
+		/**
+		 * @brief
+		 * @param user_data
+		 */
+		void (*render)(void* user_data);
+	};
+
+	struct application_desc_t
+	{
+		window_desc_t window_desc;
+		u32 updates_per_second;
+	};
+
+	struct application_t
+	{
+		application_desc_t      _desc;
+		application_callbacks_t _callbacks;
+		window_t                _window;
+
+		static application_t create(application_desc_t desc, application_callbacks_t callbacks);
+		void destroy();
+
+		void start();
+		void stop();
+		void run();
+	};
+}
